017d9483102a69836a641883953883893054ee47
Doanda37Rahma <ddoanda@gmail.com>
2023-05-10 12:18:56 +0700

    refactor: revise server launch

--- src/com/serversocket/Main.java
+++ src/com/serversocket/Main.java
@@ -4,7 +4,25 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Main {
-    static final int PORT = 80;
+    static final int SOCKET_PORT = 80;
+
+    public static void launchServer() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(SOCKET_PORT);
+            System.out.println("Socket server started at http:://127.0.0.1:" + SOCKET_PORT);
+    
+            while (true) {
+                ClientServer client = new ClientServer(serverSocket.accept());
+                client.serve();
+            }
+
+            // System.out.println("Close the server socket");
+            // serverSocket.close();
+    
+        } catch (IOException e) {
+            System.err.println("Error starting socket server: " + e.getMessage());
+        }
+    }
 
     /**
      * Run server socket
@@ -12,21 +30,8 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        try {
-            try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-                System.out.println("Server started: http:://127.0.0.1:" + PORT);
-
-                while (true) {
-                    ClientServer client = new ClientServer(serverSocket.accept());
-                    client.serve();
-                }
-            }
-
-//            System.out.println("Close the server socket");
-//            serverSocket.close();
-
-        } catch (IOException e) {
-            System.err.println("Server Connection error : " + e.getMessage());
-        }
+        launchServer();
     }
+
+    
 }
